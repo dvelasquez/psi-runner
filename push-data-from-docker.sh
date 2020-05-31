66cd1ba432c6633b38a09a7a10b5663edc08e4ae
Danilo Velasquez <danilo.velasquez@gmail.com>
2020-05-31 19:36:48 +0200

    master: updated data from docker

--- push-data-from-docker.sh
+++ push-data-from-docker.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 GIT=`which git`
+
+${GIT} config --global user.email "danilo.velasquez@gmail.com"
+${GIT} config --global user.name "Danilo Velasquez"
+${GIT} remote set-url origin "https://dvelasquez:${GITHUB_PERSONAL_API_TOKEN}@github.com/dvelasquez/psi-runner.git" > /dev/null 2>&1
+${GIT} config submodule.psi-data.url "https://dvelasquez:${GITHUB_PERSONAL_API_TOKEN}@github.com/dvelasquez/psi-data.git" > /dev/null 2>&1
+${GIT} submodule update --init --recursive
+
 REPO_DIR=/psi-runner/psi-data
 cd ${REPO_DIR}
 ls -lah
